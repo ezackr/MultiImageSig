a5fb42e40c9e750a5a676292cb542050d8b1643a
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-05-11 09:16:45 -0700

    implemented transformer encoder

---
+++ src/main/models/__init__.py
@@ -0,0 +1,2 @@
+from src.main.models.position import PositionalEncoding
+from src.main.models.transformer import Encoder, EncoderBlock
--- src/main/models/transformer.py
+++ src/main/models/transformer.py
@@ -1,17 +1,75 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from typing import Tuple
+
+from src.main.models.position import PositionalEncoding
 
 
-class Transformer(nn.Module):
+class EncoderBlock(nn.Module):
     """
-    Transformer to compute output in a number of classes (numClasses) from image signature input (of dimension inputDim)
-    """
-    def __init__(self, inputDim : Tuple[int, int], numClasses : int = 10):
-      super(Transformer, self).__init__()
-      # TODO
+    A single encoder block for the transformer encoder. Includes a
+    MultiHeadAttention layer followed by a FeedForward layer.
 
-    def forward(self, x : torch.tensor):
-       # TODO
-       pass
+    Parameters:
+        - d_model (int): dimension of input.
+        - num_heads (int): number of attention heads.
+        - dropout (float): dropout probability.
+    """
+    def __init__(self, d_model: int, num_heads: int = 8, dropout: float = 0.1):
+        super(EncoderBlock, self).__init__()
+        # self-attention.
+        self.attention = nn.MultiheadAttention(embed_dim=d_model,
+                                               num_heads=num_heads)
+        self.norm1 = nn.LayerNorm(normalized_shape=d_model)
+        self.dropout1 = nn.Dropout(p=dropout)
+        # feed forward.
+        self.feedforward = nn.Sequential(
+            nn.Linear(d_model, 64),
+            nn.ReLU(),
+            nn.Linear(64, d_model)
+        )
+        self.norm2 = nn.LayerNorm(normalized_shape=d_model)
+        self.dropout2 = nn.Dropout(p=dropout)
+
+    def forward(self, x: torch.Tensor):
+        x_attn = self.dropout1(self.norm1(x + self.attention(x, x, x)[0]))
+        x_out = self.dropout2(self.norm2(x_attn + self.feedforward(x_attn)))
+        return x_out
+
+
+class Encoder(nn.Module):
+    """
+    A sequence of EncoderBlocks used to create an encoder.
+
+    Parameters:
+        - num_layers (int): number of encoder blocks.
+        - d_model (int): dimension of input.
+        - num_heads (int): number of attention heads.
+        - num_classes (int): number of output classes.
+        - max_len (int): length of the input sequence.
+        - dropout (float): dropout probability.
+    """
+    def __init__(
+            self,
+            num_layers: int = 6,
+            d_model: int = 512,
+            num_heads: int = 8,
+            num_classes: int = 10,
+            max_len: int = 1024,
+            dropout: float = 0.1
+    ):
+        super(Encoder, self).__init__()
+        self.positional_encoding = PositionalEncoding(max_len, d_model)
+        self.blocks = nn.ModuleList([
+          EncoderBlock(d_model, num_heads, dropout)
+          for _ in range(num_layers)
+        ])
+        self.linear = nn.Linear(d_model, num_classes)
+
+    def forward(self, x: torch.Tensor):
+        x_enc = self.positional_encoding(x)
+        for block in self.blocks:
+            x_enc = block(x_enc)
+        x_out = self.linear(x_enc)
+        x_out = F.softmax(x_out)
+        return x_out
