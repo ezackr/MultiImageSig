82c8adb68294c264a38fa6bbc791332fcba8809c
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-05-11 09:21:33 -0700

    linted CNN

--- src/main/models/cnn.py
+++ src/main/models/cnn.py
@@ -8,25 +8,25 @@ class CNN(nn.Module):
     """
     CNN to compute output in a number of classes (numClasses) from image signature input (of dimension inputDim)
     """
-    def __init__(self, inputDim : Tuple[int, int], hiddenN : int = 50, numClasses : int = 10):
-      super(CNN, self).__init__()
-      # TODO: implement correct filters
-      self.conv1 = nn.Conv1d(3, 32, kernel_size=3)
-      self.conv2 = nn.Conv1d(32, 64, kernel_size=3)
-      self.fc1 = nn.Linear(inputDim[0] * inputDim[1], hiddenN)
-      self.fc2 = nn.Linear(hiddenN, numClasses)
+    def __init__(self, inputDim: Tuple[int, int], hiddenN: int = 50, numClasses: int = 10):
+        super(CNN, self).__init__()
+        # TODO: implement correct filters
+        self.conv1 = nn.Conv1d(3, 32, kernel_size=3)
+        self.conv2 = nn.Conv1d(32, 64, kernel_size=3)
+        self.fc1 = nn.Linear(inputDim[0] * inputDim[1], hiddenN)
+        self.fc2 = nn.Linear(hiddenN, numClasses)
 
     def forward(self, x : torch.tensor):
-       x = self.conv1(x)
-       x = F.relu(x)
-       x = F.max_pool1d(3)
-       x = self.conv2(x)
-       x = F.relu(x)
-       x = F.max_pool1d(3)
-       x = torch.flatten(x, 1)
-       x = self.fc1(x)
-       x = F.relu(x)
-       x = F.dropout(x, 0.4)
-       x = self.fc2(x)
-       out = F.log_softmax(x, dim=1)
-       return out
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool1d(3)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool1d(3)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = F.dropout(x, 0.4)
+        x = self.fc2(x)
+        out = F.log_softmax(x, dim=1)
+        return out
