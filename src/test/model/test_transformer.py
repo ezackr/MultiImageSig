d8e9ae2eb0b2ecbb009fecc1c77a364b129c0e90
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-05-13 11:55:56 -0700

    updated transformer test to match new behavior

--- src/test/model/test_transformer.py
+++ src/test/model/test_transformer.py
@@ -16,11 +16,13 @@ def test_encoder_block():
 
 def test_encoder():
     batch_size = 32
+    input_dim = 120
     d_model = 512
     seq_len = 256
-    x = torch.rand(size=(batch_size, seq_len, d_model))
+    x = torch.rand(size=(batch_size, seq_len, input_dim))
 
     encoder = Encoder(
+        input_dim=input_dim,
         num_layers=6,
         d_model=d_model,
         num_heads=8,
