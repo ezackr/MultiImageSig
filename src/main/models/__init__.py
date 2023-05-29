172d8fec7714d906e6c8c4d5e1f277964ade7b55
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-05-28 17:02:59 -0700

    fixed attention encoder to match original multiimagesig.

--- src/main/models/__init__.py
+++ src/main/models/__init__.py
@@ -1,4 +1,3 @@
 from src.main.models.cnn import CNN
 from src.main.models.fc import FC
-from src.main.models.position import PositionalEncoding
 from src.main.models.transformer import AttentionEncoder, EncoderBlock
--- src/main/models/transformer.py
+++ src/main/models/transformer.py
@@ -1,8 +1,9 @@
+from math import floor
+from typing import Tuple
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-from src.main.models.position import PositionalEncoding
 
 
 class EncoderBlock(nn.Module):
@@ -47,42 +48,77 @@ class EncoderBlock(nn.Module):
 
 class AttentionEncoder(nn.Module):
     """
-    A sequence of EncoderBlocks used to create an encoder.
+    An attention-based encoder for image classification. Applies two
+    convolutional layers, a sequence of transformer blocks, and then
+    feed-forward layers for classification.
 
     Parameters:
-        - input_dim (int): the dimension of the input.
+        - input_shape (Tuple[int, int]): the dimension of the input. The tuple
+            represents (seq_len, input_dim).
         - num_layers (int): number of encoder blocks.
-        - d_model (int): dimension of input.
         - num_heads (int): number of attention heads.
         - num_classes (int): number of output classes.
-        - max_len (int): length of the input sequence.
         - dropout (float): dropout probability.
     """
     def __init__(
             self,
-            input_dim: int,
+            input_shape: Tuple[int, int],
             num_layers: int = 6,
-            d_model: int = 512,
             num_heads: int = 8,
             num_classes: int = 10,
-            max_len: int = 256,
             dropout: float = 0.1
     ):
         super(AttentionEncoder, self).__init__()
-        self.linear_in = nn.Linear(input_dim, d_model)
-        self.positional_encoding = PositionalEncoding(max_len, d_model)
-        self.blocks = nn.ModuleList([
-          EncoderBlock(d_model, num_heads, dropout)
-          for _ in range(num_layers)
-        ])
-        self.linear_out = nn.Linear(d_model, num_classes)
+        seq_len, sig_dim = input_shape
+        # convolutional layers.
+        self.conv1 = nn.Conv1d(
+            in_channels=sig_dim,
+            out_channels=32,
+            kernel_size=(3,),
+        )
+        self.pool1 = nn.MaxPool1d(kernel_size=3)
+        self.seq_len = floor((((seq_len - 2) - 3) // 3) + 1)
+        self.conv2 = nn.Conv1d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=(3,)
+        )
+        self.pool2 = nn.MaxPool1d(kernel_size=3)
+        self.seq_len = floor((((self.seq_len - 2) - 3) // 3) + 1)
+
+        # transformer blocks.
+        self.encoder_blocks = nn.ModuleList()
+        for _ in range(num_layers):
+            self.encoder_blocks.append(EncoderBlock(
+                d_model=64,
+                num_heads=num_heads,
+                dropout=dropout
+            ))
+
+        # fully-connected layers.
+        self.norm = nn.LayerNorm(normalized_shape=64)
+        self.dropout = nn.Dropout(p=dropout)
+        self.encoder_linear = nn.Linear(in_features=64, out_features=1)
+        self.out_linear = nn.Sequential(
+            nn.Linear(in_features=64, out_features=50),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.Linear(in_features=50, out_features=num_classes),
+            nn.Softmax()
+        )
 
     def forward(self, x: torch.Tensor):
-        print(x.shape)
-        x_in = self.linear_in(x)
-        x_enc = self.positional_encoding(x_in)
-        for block in self.blocks:
+        # convolutional layers.
+        x = x.permute(0, 2, 1)
+        x_enc = self.pool1(self.conv1(x))
+        x_enc = self.pool2(self.conv2(x_enc))
+        x_enc = x_enc.permute(0, 2, 1)
+        # transformer layers.
+        for block in self.encoder_blocks:
             x_enc = block(x_enc)
-        x_out = self.linear_out(x_enc)
-        x_out = F.softmax(x_out)
+        # fully-connected layers.
+        x_rep = self.dropout(self.norm(x_enc))
+        x_attn = F.softmax(self.encoder_linear(x_rep), dim=1)
+        x_out = torch.matmul(x_attn.permute(0, 2, 1), x_rep).squeeze(dim=1)
+        x_out = self.out_linear(x_out)
         return x_out
